a1c2dc7d026396a92fe02900577d78b9c16dc9b5
theambidextrous <iotuya05@gmail.com>
2019-05-19 16:10:38 +0300

    index key on tbl customer

--- db/imovies.sql
+++ db/imovies.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 17, 2019 at 04:21 PM
+-- Generation Time: May 19, 2019 at 01:08 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -30,14 +30,22 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
-  `CustomerId` varchar(50) NOT NULL,
-  `Username` varchar(50) NOT NULL,
+  `CustomerId` int(50) NOT NULL AUTO_INCREMENT,
+  `Fullname` varchar(50) NOT NULL,
   `EmailAddress` varchar(50) NOT NULL,
   `Password` varchar(200) NOT NULL,
   `PhoneNumber` varchar(50) NOT NULL,
   `Status` varchar(50) NOT NULL,
   PRIMARY KEY (`CustomerId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`CustomerId`, `Fullname`, `EmailAddress`, `Password`, `PhoneNumber`, `Status`) VALUES
+(1, ' ', 'apis@bytebladesystems.com', 'jjjj', '98876666', '1'),
+(2, 'idd juma', 'apis@bytebladesystems.com', 'juma', '0705007984', '1');
 
 -- --------------------------------------------------------
 
